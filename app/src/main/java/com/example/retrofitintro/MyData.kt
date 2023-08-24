b4ddcb3a083d4bfd451f3ded62359a1995136708
Antonio Saracino <antonio.saracino83@gmail.com>
2023-08-24 19:14:10 +0200

    Retrofit training

---
+++ app/src/main/java/com/example/retrofitintro/MyData.kt
@@ -0,0 +1,3 @@
+package com.example.retrofitintro
+
+class MyData : ArrayList<MyDataItem>()
---
+++ app/src/main/java/com/example/retrofitintro/MyDataItem.kt
@@ -0,0 +1,9 @@
+package com.example.retrofitintro
+
+data class MyDataItem(
+    val body: String,
+    val id: Int,
+    val title: String,
+    val userId: Int
+
+)
